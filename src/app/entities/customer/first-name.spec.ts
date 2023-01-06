94bd8144e3fa5037b99d7461ab912ef303b8d9c5
adam-almeida <adamportifolio@gmail.com>
2023-01-05 22:21:35 -0300

    test: create customer firstname more 50 character

--- src/app/entities/customer/first-name.spec.ts
+++ src/app/entities/customer/first-name.spec.ts
@@ -8,3 +8,7 @@ test('it should be able to create a customer first name', () => {
 test('it should not be able to create a customer first name with less than 3 characters', () => {
   expect(() => new FirstName('Aa')).toThrow()
 })
+
+test('it should not be able to create a customer first name with more than 50 characters', () => {
+    expect(() => new FirstName('A'.repeat(51))).toThrow()
+  })
