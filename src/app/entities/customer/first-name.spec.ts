463760c7475792e987231ca99ab6dff06a726ef8
adam-almeida <adamportifolio@gmail.com>
2023-01-05 22:19:33 -0300

    test: create customer first name less 3 characters

--- src/app/entities/customer/first-name.spec.ts
+++ src/app/entities/customer/first-name.spec.ts
@@ -2,6 +2,9 @@ import { FirstName } from './fisrst-name'
 
 test('it should be able to create a customer first name', () => {
   const firstName = new FirstName('Adam Almeida')
-
   expect(firstName).toBeTruthy()
+})
+
+test('it should not be able to create a customer first name with less than 3 characters', () => {
+  expect(() => new FirstName('Aa')).toThrow()
 })
