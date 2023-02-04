68ac727cfde2f82e71e6738d028f16a99e3fb05a
adam-almeida <adamportifolio@gmail.com>
2023-02-04 14:46:57 -0300

    feat: create customer post route

--- src/infra/routes/customer-routes.ts
+++ src/infra/routes/customer-routes.ts
@@ -1,10 +1,27 @@
+import { CreateCustomer } from '@/app/use-cases/create-customer'
 import { Router, Request, Response } from 'express'
 import { prisma } from '../prisma-service'
 
 export const customer = Router()
 
-customer.get('/customer', (request: Request, response: Response) => {
-  const customers = prisma.customer.findMany()
+customer.post('/customer', async (request: Request, response: Response) => {
+    const { firstName, lastName, email, password, userType } = request.body
 
-  response.send({ message: 'Hello World', customers })
+    const createCustomerUseCase = new CreateCustomer()
+
+    const customer = await createCustomerUseCase.execute({
+        firstName,
+        lastName,
+        email,
+        password,
+        userType,
+    })
+
+    response.json(customer)
+})
+
+customer.get('/customer', (request: Request, response: Response) => {
+    const customers = prisma.customer.findMany()
+
+    response.send({ message: 'Hello World', customers })
 })
