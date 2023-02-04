d59ed36e9148abf42e35d488e73b1bf15b1d4177
adam-almeida <adamportifolio@gmail.com>
2023-02-04 17:00:19 -0300

    test: create a new customer with an admin role

--- src/app/entities/customer/customer.spec.ts
+++ src/app/entities/customer/customer.spec.ts
@@ -2,6 +2,7 @@ import { Customer } from './customer'
 import { FirstName } from './first-name'
 import { LastName } from './last-name'
 import { Email } from './email'
+import { Role } from '@/app/use-cases/create-customer'
 
 describe('Customers', () => {
     //deve ser possível criar um novo cliente
@@ -14,16 +15,27 @@ describe('Customers', () => {
         })
         expect(customer).toBeTruthy()
     }),
-
-    //deve ser possível criar um novo cliente com o status ativo de email
-    it('should be able to create a new customer with an active email status', () => {
-        const customer = new Customer({
-            firstName: new FirstName('Adam'),
-            lastName: new LastName('Almeida'),
-            emailActived: true,
-            email: new Email('test@email.com'),
-            password: '12345678',
+        //deve ser possível criar um novo cliente com o status ativo de email
+        it('should be able to create a new customer with an active email status', () => {
+            const customer = new Customer({
+                firstName: new FirstName('Adam'),
+                lastName: new LastName('Almeida'),
+                emailActived: true,
+                email: new Email('test@email.com'),
+                password: '12345678',
+            })
+            expect(customer).toBeTruthy()
+        }),
+        //deve ser possível criar um novo cliente com a permissão de ADMIN
+        it('should be able to create a new customer with an ADMIN role', () => {
+            const customer = new Customer({
+                firstName: new FirstName('Adam'),
+                lastName: new LastName('Almeida'),
+                emailActived: true,
+                email: new Email('test@email.com'),
+                password: '12345678',
+                userType: Role.ADMIN,
+            })
+            expect(customer).toBeTruthy()
         })
-        expect(customer).toBeTruthy()
-    })
 })
